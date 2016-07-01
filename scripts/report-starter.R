11d38cfb17f4fc55f85685fc945a8390e36ed002
Andrey Koval <koval.andrey@gmail.com>
2016-07-01 08:31:09 -0700

    nice clean starters

---
+++ scripts/report-starter.R
@@ -0,0 +1,38 @@
+# knitr::stitch_rmd(script="./___/___.R", output="./___/___/___.md")
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+cat("\f") # clear console 
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) # enables piping : %>% 
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("ggplot2") # graphing
+requireNamespace("readr")   # data input
+requireNamespace("tidyr")   # data manipulation
+requireNamespace("dplyr")   # Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("testit")  # For asserting conditions meet expected patterns.
+# requireNamespace("car")     # For it's `recode()` function.
+
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+source("./scripts/common-functions.R") # used in multiple reports
+source("./scripts/graph-presets.R") # fonts, colors, themes 
+
+
+# ---- declare-globals ---------------------------------------------------------
+
+# ---- load-data ---------------------------------------------------------------
+
+# ---- inspect-data -------------------------------------------------------------
+
+# ---- tweak-data --------------------------------------------------------------
+
+# ---- basic-table --------------------------------------------------------------
+
+# ---- basic-graph --------------------------------------------------------------
+
+# ---- reproduce ---------------------------------------
+rmarkdown::render(input = "./sandbox/report-a.Rmd" ,
+                  output_format="html_document", clean=TRUE)
