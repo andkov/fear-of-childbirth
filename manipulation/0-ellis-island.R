c618fb2abdd6652be79194487203feaa19f3560d
Andrey Koval <koval.andrey@gmail.com>
2016-07-14 10:33:42 -0700

    #1 ellis island complete

--- manipulation/0-ellis-island.R
+++ manipulation/0-ellis-island.R
@@ -10,6 +10,7 @@ cat("\f") # clear console
 # Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
 library(magrittr) # enables piping : %>% 
 # Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("foreign") # for importing SPSS files
 requireNamespace("ggplot2") # graphing
 requireNamespace("readr")   # data input
 requireNamespace("tidyr")   # data manipulation
@@ -23,24 +24,23 @@ source("./scripts/common-functions.R") # used in multiple reports
 source("./scripts/graph-presets.R") # fonts, colors, themes 
 
 # ---- declare-globals ---------------------------------------------------------
-filePath <- "./data/unshared/Final Dataset NFSept 6 2015_1.sav"
+data_path_input  <- "./data/unshared/raw/Final Dataset NFSept 6 2015_1.sav"
 
-# ---- load-data ---------------------------------------------------------------
-ds0 <- Hmisc::spss.get(filePath, use.value.labels = TRUE) 
-
+# ---- load-data ------------------------------------------------
+# load data objects
+unitData <- foreign::read.spss(data_path_input,to.data.frame = TRUE) 
+ 
 # ---- inspect-data -------------------------------------------------------------
-nl <- names_labels(ds0)
-
+nl <- names_labels(unitData)
 
 # ---- basic-table --------------------------------------------------------------
 
 # ---- basic-graph --------------------------------------------------------------
 
-
-# ---- collect-meta-data -----------------------------------------
+# ---- collect-meta-data-live -----------------------------------------
 ## we begin by extracting the names and (hopefuly their) labels of variables from each dataset
 # and combine them in a single rectanguar object, long/stacked with respect to study names
-nl <- names_labels(ds0)
+nl <- names_labels(unitData)
 readr::write_csv(nl,"./data/shared/meta/meta-data-live.csv")
 
 # ----- import-meta-data-dead -----------------------------------------
@@ -48,14 +48,11 @@ readr::write_csv(nl,"./data/shared/meta/meta-data-live.csv")
 # we made a dead copy of `./data/shared/derived/meta-raw-live.csv` and named it `./data/shared/meta-data-map.csv`
 # decisions on variables' renaming and classification is encoded in this map
 # reproduce ellis-island script every time you make changes to `meta-data-map.csv`
-meta <- read.csv("./data/shared/meta/meta-data-dead.csv")
-meta <- meta %>% dplyr::filter(select==TRUE)
-variable_names <- as.character(meta[,"name"])
+metaData <- read.csv("./data/shared/meta/meta-data-dead.csv")
 
-ds <- ds0 %>% 
-  dplyr::select_(.dots = variable_names)
-names(ds) <- as.character(meta[,"name_new"])
-names(ds) 
+# ---- inspect-data ----------------------------------------------
+# inspect loaded data objects (using basic demographic variables )
+
 
 # ---- tweak-data --------------------------------------------------------------
 
@@ -63,11 +60,21 @@ names(ds)
 # ---- save-to-disk ------------------------------------------------------------
 
 dto <- list()
-dto[["UnitData"]] <- ds
-dto[['MetaData']] <- meta
+dto[["unitData"]] <- unitData
+dto[['metaData']] <- metaData
 # Save as a compress, binary R dataset.  It's no longer readable with a text editor, but it saves metadata (eg, factor information).
 saveRDS(dto, file="./data/unshared/derived/dto.rds", compress="xz")
 
+
+# ---- object-verification ------------------------------------------------
+# the production of the dto object is now complete
+# we verify its structure and content:
+dto <- readRDS("./data/unshared/derived/dto.rds")
+names(dto)
+# 1st element - unit(person) level data
+names(dto[["unitData"]])
+# 2nd element - meta data, info about variables
+names(dto[["metaData"]])
 
 
 
